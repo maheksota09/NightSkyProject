9e208c512a3c89744a2b57eb894edefc06c3ca4d
alamgir09 <alamgir@usc.edu>
2023-06-29 13:27:36 +0300

    changed endpoint

--- flask-server/server.py
+++ flask-server/server.py
@@ -3,9 +3,9 @@ from flask import Flask
 app = Flask(__name__)
 
 #Login Endpoint
-@app.route("/login")
+@app.route("/")
 def login(): 
     return {"Hello" : ["Hello World! You've entered the login page"]}
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host='0.0.0.0', debug=True)
